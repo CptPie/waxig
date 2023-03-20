67767821c9973333626ce18a068d2589126d1697
CptPie <23438606+CptPie@users.noreply.github.com>
2023-03-21 00:54:53 +0100

    Lexer and Tokens done

---
+++ interpreter/token/token.go
@@ -0,0 +1,72 @@
+package token
+
+import "strings"
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "=" // Assignment
+	PLUS     = "+" // Addition
+	MINUS    = "-" // Subtraction
+	ASTERISK = "*" // Multiplication
+	SLASH    = "/" // Division
+	HAT      = "^" // Exponentiation
+	BANG     = "!" // Negation
+
+	// Boolean operators
+	EQ     = "==" // Equal to
+	NOT_EQ = "!=" // Not equal to
+	LT     = "<"  // Less than
+	GT     = ">"  // Greater than
+	LTEQ   = "<=" // Less than or equal to
+	GTEQ   = ">=" // Greater than or equal to
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	// Check if the identifier is a keyword, case-insensitive
+	if tok, ok := keywords[strings.ToLower(ident)]; ok {
+		return tok
+	}
+	return IDENT
+}
