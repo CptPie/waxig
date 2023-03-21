7ef63e9790706dccd8403a2597830f5b51ad3129
CptPie <23438606+CptPie@users.noreply.github.com>
2023-03-21 01:26:05 +0100

    Expanding the AST prior to the parser work

---
+++ interpreter/ast/ast_test.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"testing"
+	"waixg/interpreter/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	expected := "let myVar = anotherVar;"
+
+	if program.String() != expected {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
