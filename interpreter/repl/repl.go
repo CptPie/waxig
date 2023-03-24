108d1069a83953250f017113dcf003856265b596
CptPie <23438606+CptPie@users.noreply.github.com>
2023-03-24 01:52:48 +0100

    Turning the RLPL into a RPPL

--- interpreter/repl/repl.go
+++ interpreter/repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"waixg/interpreter/lexer"
-	"waixg/interpreter/token"
+	"waixg/interpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -22,10 +22,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			_, _ = fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
 
+		_, _ = io.WriteString(out, program.String())
+		_, _ = io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []error) {
+	for _, msg := range errors {
+		_, _ = fmt.Fprintf(out, "\t%s\n", msg)
 	}
 }
